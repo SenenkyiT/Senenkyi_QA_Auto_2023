09fc3eaaac1251bbbfb6a5c132373a7304aba98a
Taras Senenkyi <senenkyi.t@gmail.com>
2023-01-29 13:38:44 +0200

    Test name changed to personal name

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,8 +8,8 @@ class User:
         self.second_name = None
 
     def create(self):
-        self.name = "Sergii"
-        self.second_name = "Butenko"
+        self.name = "Taras"
+        self.second_name = "Senenkyi"
 
     def remove(self):
         self.name = ""
