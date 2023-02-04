045730157493cc8cf0dbd27ed4babedcf6677cf9
Taras Senenkyi <senenkyi.t@gmail.com>
2023-02-04 18:59:55 +0200

    api client created, github api tests added, changes in other files

---
+++ tests/api/test_github_api.py
@@ -0,0 +1,34 @@
+import pytest
+from modules.api.clients.github import GitHub
+
+
+@pytest.mark.api
+def test_user_exists(github_api):
+    user = github_api.get_user('defunkt')
+    assert user['login'] == 'defunkt'
+
+
+@pytest.mark.api
+def test_user_not_exists(github_api):
+    r = github_api.get_user('butenkosergii')
+    #print(r)
+    assert r['message'] == 'Not Found'
+
+@pytest.mark.api
+def test_repo_can_be_found(github_api):
+    r = github_api.search_repo('become-qa-auto')
+    #print(r)
+    assert r['total_count'] == 26
+    ###перевірка, що перший знайдений репозиторій містить ім'я 'become-qa-auto'
+    assert 'become-qa-auto' in r['items'][0]['name']
+
+@pytest.mark.api
+def test_repo_cannot_be_foud(github_api):
+    r = github_api.search_repo('sergiibutenko_repo_non_exist')
+    assert r['total_count'] == 0
+
+
+@pytest.mark.api
+def test_repo_with_single_cahr_be_found(github_api):
+    r = github_api.search_repo('s')
+    assert r['total_count'] != 0
